3411ddedf12e9f3a01799ff3fe032e7d17a1e5e7
Nicolás Doyhenart <nidoyhe@gmail.com>
2024-07-04 10:37:33 -0300

    Commit 04/07 10:37am

---
+++ efectos_de_sonido.py
@@ -0,0 +1,30 @@
+"""""
+TP GRUPAL PYGAME
+
+Intregrantes: Nicolás Doyhenart, Santino Fernandez
+"""""
+import pygame
+
+def efecto_de_sonido():
+    pygame.mixer.init()
+    sonido_fondo = pygame.mixer.Sound(r"recursos\pop.mp3")
+    sonido_fondo.set_volume(0.7)
+    sonido_fondo.play()
+
+def derrota():
+    pygame.mixer.init()
+    sonido_fondo = pygame.mixer.Sound(r"recursos\error.mp3")
+    sonido_fondo.set_volume(0.7)
+    sonido_fondo.play()
+
+def comodin_usado():
+    pygame.mixer.init()
+    sonido_fondo = pygame.mixer.Sound(r"recursos\error windows.mp3")
+    sonido_fondo.set_volume(3.0)
+    sonido_fondo.play()
+
+def respuesta_correcta_sonido():
+    pygame.mixer.init()
+    sonido_fondo = pygame.mixer.Sound(r"recursos\respuesta_correcta.mp3")
+    sonido_fondo.set_volume(0.7)
+    sonido_fondo.play()
